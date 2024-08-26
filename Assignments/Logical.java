fb6f931bbd79a4d034b421c073f5f730f4ffbcbb
Tanmay-Vaidya <tanmaymvaidya@gmail.com>
2024-08-26 13:44:06 +0530

    Write a Java program that accepts three numbers from the user and checks if all three numbers are equal using logical operators. If they are, print "All numbers are equal"; otherwise, print "Numbers are not equal."

---
+++ Assignments/Logical.java
@@ -0,0 +1,34 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class Logical {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        if (num1 == num2 && num2 == num3) {
+            System.out.println("All numbers are equal.");
+        } else {
+            System.out.println("Numbers are not equal.");
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
